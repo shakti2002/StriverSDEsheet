58e5736b814ee52c152b632bb9d815a7fb15a8fe
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2023-06-13 04:09:54 -0700

    Create 13-06-2023.cpp

---
+++ 2_pascals_triangle/13-06-2023.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+     vector<int> getRow(int rowIndex) {
+        long long ans=1;
+        vector<int>ansRow;
+        ansRow.push_back(1);
+        for(int col=1; col<rowIndex; col++){
+            ans=ans*(rowIndex-col);
+            ans=ans/(col);
+            ansRow.push_back(ans);
+        }
+        return  ansRow;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+       for(int i=1; i<=numRows; i++){
+           ans.push_back(getRow(i));
+       }
+       return ans;
+    }
+};
